0168669f71862cb61730108b20aa6c0d195d37b0
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2023-10-16 19:58:00 -0300

    modified cors whitelist

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,7 @@
 import { type CorsOptions } from 'cors'
 import { CustomError } from '../domain/errors/customError.js'
 
-const whitelist = ['http://localhost:5173', 'http://127.0.0.1:5173']
+const whitelist = ['https://react-auth-example-sandy.vercel.app/', 'https://react-auth-example-sandy.vercel.app']
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     if (typeof origin === 'string' && (whitelist.includes(origin) || origin === undefined)) {
